89514dee658237cfd989a5279c67558c60610b27
marcel-etomni <marcel@etomni.com>
2026-04-11 18:07:37 +0100

    Delete et-omni-go-symphony directory

--- et-omni-go-symphony/scripts/polyfill.js
+++
@@ -1,9 +0,0 @@
-if (typeof global.CustomEvent === "undefined") {
-  global.CustomEvent = class CustomEvent {
-    constructor(type, params = {}) {
-      this.type = type;
-      this.detail = params.detail;
-    }
-  };
-}
-``
